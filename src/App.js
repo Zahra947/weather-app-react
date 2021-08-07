43829274b22d7202dd8b3246be95334169400906
Zahra947 <zhr.rabiei@gmail.com>
2021-08-07 11:49:26 +0430

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      This is the first app with react
+      <div className="contaiiner">
+        Weather App This is the first app with react
+      </div>
     </div>
   );
 }
