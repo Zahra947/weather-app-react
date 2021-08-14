2f791eaa3f5a6d76ed6dbce642094b9a9f9680f9
Zahra947 <zhr.rabiei@gmail.com>
2021-08-14 10:33:35 +0430

    changed the props name

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,8 +15,16 @@ export default function WeatherForecast(props) {
 
   if (loaded) {
     return (
-      <div className="row" id="forecast">
-        <WeatherForecastDay data={forecast[0]} />
+      <div className="row">
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 5) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
+          }
+        })}
       </div>
     );
   } else {
